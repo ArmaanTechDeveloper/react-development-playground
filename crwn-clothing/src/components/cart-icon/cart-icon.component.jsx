d92f0cde9afc6a8f8b182e2a09f666834284a2de
ArmaanTechDeveloper <kournarinder948@gmail.com>
2023-12-09 22:01:12 +0530

    updated till REDUCERS #149

--- crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
+++ crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
@@ -1,19 +1,22 @@
-import './cart-icon.styles.scss'
-import {ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
+import { useContext } from 'react';
 
-import { useContext } from 'react'
-import { CartContext } from '../../contexts/cart.context'
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+
+import { CartContext } from '../../contexts/cart.context';
+
+import { CartIconContainer, ItemCount } from './cart-icon.styles';
 
 const CartIcon = () => {
-    const {isCartOpen , setIsCartOpen , cartItemCount} = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
-    const toogleIsCartOpen = () => setIsCartOpen(!isCartOpen);
-    return(
-        <div className='cart-icon-container' onClick={toogleIsCartOpen}>
-            <ShoppingIcon className='shopping-icon' />
-            <span className='item-count'>{cartItemCount}</span>
-        </div>
-    )
-}
+  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+
+  return (
+    <CartIconContainer onClick={toggleIsCartOpen}>
+      <ShoppingIcon className='shopping-icon' />
+      <ItemCount>{cartCount}</ItemCount>
+    </CartIconContainer>
+  );
+};
 
 export default CartIcon;
