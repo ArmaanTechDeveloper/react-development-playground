cd997d4ac5cde12ea63f92996a4949b7766207ac
ArmaanTechDeveloper <kournarinder948@gmail.com>
2023-12-22 00:48:28 +0530

    redux-persist and redux-thunk implementation

--- crwn-clothing/src/routes/category/category.component.jsx
+++ crwn-clothing/src/routes/category/category.component.jsx
@@ -3,7 +3,8 @@ import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import ProductCard from '../../components/product-card/product-card.component';
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import Spinner from '../../components/spinner/spinner.component';
+import { selectCategoriesIsLoading, selectCategoriesMap } from '../../store/categories/category.selector';
 
 
 import { CategoryContainer, Title } from './category.styles';
@@ -11,6 +12,8 @@ import { CategoryContainer, Title } from './category.styles';
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap)
+
+  const isLoading = useSelector(selectCategoriesIsLoading)
 
   const [products, setProducts] = useState(categoriesMap[category]);
 
@@ -21,12 +24,14 @@ const Category = () => {
   return (
     <Fragment>
       <Title>{category.toUpperCase()}</Title>
-      <CategoryContainer>
+      {
+        isLoading? (<Spinner />) : (<CategoryContainer>
         {products &&
           products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
-      </CategoryContainer>
+      </CategoryContainer>)
+      }
     </Fragment>
   );
 };
--- crwn-clothing/src/store/store.js
+++ crwn-clothing/src/store/store.js
@@ -2,9 +2,22 @@
 import logger from 'redux-logger'
 import { rootReducer } from './root-reducer'
 import { createStore , compose , applyMiddleware} from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { thunk } from 'redux-thunk'
 
-const middleWares = [logger]
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
+
+const persistedReducer = persistReducer(persistConfig , rootReducer)
+
+const middleWares = [logger , thunk]
 
 const composedEnhancers =  compose(applyMiddleware(...middleWares))
 
-export const store = createStore(rootReducer, undefined , composedEnhancers)
+export const store = createStore(persistedReducer, undefined , composedEnhancers)
+
+export const persistor = persistStore(store)
