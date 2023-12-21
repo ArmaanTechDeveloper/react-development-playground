6db64a5992a9fad6ac0e89a1a15946dafe7a1f79
ArmaanTechDeveloper <kournarinder948@gmail.com>
2023-12-21 19:49:50 +0530

    migrated everything to redux

--- crwn-clothing/src/routes/shop/shop.component.jsx
+++ crwn-clothing/src/routes/shop/shop.component.jsx
@@ -2,8 +2,23 @@ import { Routes, Route } from 'react-router-dom';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
+import { useEffect } from 'react';
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+import { setCategories } from '../../store/categories/category.action'
+import { useDispatch } from 'react-redux';
 
 const Shop = () => {
+
+  const dispatch = useDispatch()
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoriesArray = await getCategoriesAndDocuments('categories');
+      dispatch(setCategories(categoriesArray));
+    };
+
+    getCategoriesMap();
+  }, []);
+
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
