2f7ec490143391bc66a5062a0e8f0ade0f0c2ded
ArmaanTechDeveloper <kournarinder948@gmail.com>
2023-12-31 00:31:41 +0530

    added redux persist to persist the state of the app

--- task-list/src/store/store.js
+++ task-list/src/store/store.js
@@ -1,11 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./root-reducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import logger from 'redux-logger'
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig , rootReducer)
 
 const middlewares = [logger]
 
 export const store = configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
     middleware: () => (middlewares)
 })
+
+export const persistor = persistStore(store)
